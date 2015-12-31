dd2602bfbff0137efb8c402c82234278ba65a03b
jackhasakeyboard <jackhasakeyboard@gmail.com>
2015-12-30 21:10:05 -0700

    Added bare bones scaffolding for notes with user auth and associations, home page for new users. Has tribulations with devise views being converted to haml

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  devise_for :views
+  devise_for :users
+  resources :notes
+  root 'notes#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
